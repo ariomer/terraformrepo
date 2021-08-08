45cba1b8b3b96aea7e04d27928b1d94141f4e009
ariomer <ariomer@gmail.com>
2021-08-08 21:51:27 +0100

    final3

--- modules/web-server/main.tf
+++ modules/web-server/main.tf
@@ -79,7 +79,7 @@ connection {
   provisioner "remote-exec" {
 
       inline = [
-        "sudo yum update -y", "sudm install docker -y","sudo systemctl start docker","sudo docker run -d -p 80:80 ariomer/jenkins:latest"
+        "sudo yum update -y", "sudo install docker -y","sudo systemctl start docker","sudo docker run -d -p 80:80 ariomer/jenkins:latest"
       ]
     
 }
